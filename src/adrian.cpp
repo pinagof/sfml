09412bbc320f5dec592feafe3a101be762c5dfee
fornies08 <forniesbua@ieselaios.com>
2026-04-16 07:40:58 +0000

    V2.0 fornies08  adrian.cpp

--- src/adrian.cpp
+++ src/adrian.cpp
@@ -1,27 +1,33 @@
 #include "funciones.h"
-    Adrian::Adrian(sf::Vector2f posicion,)
-    : velocidad(150.f), listo(false)
 
-    if (!textura.loadFromFile("sprite5.png"))
+Adrian::Adrian(sf::Vector2f posicion)
+    : velocidad(150.f), listo(false)
+{
+   if (!textura.loadFromFile("assets/sprite4-1.png"))
         return; // listo queda en false
 
     textura.setSmooth(false);
     sprite.setTexture(textura);
     sprite.setScale(1.f, 1.f);
-    sprite.setOrigin(textura.getSize().x / 2.f, textura.getSize().x;
-    sprite.setPosition(posición);
-    listo=false;
+    sprite.setOrigin(textura.getSize().x / 2.f, textura.getSize().y / 2.f);
+    sprite.setPosition(posicion);
+    listo = true;
+}
 
-    bool Adrian::estaListo() const { return listo;}
-    //Movimiento absoluto
-    void Adrian::setPosicion(sf::Vector2f posicion)
-    {
-        sprite.setPosicion(posicion);
-    }
+bool Adrian::estaListo() const { return listo; }
 
-    //Movimiento relativo (llamar cada frame con dt)
-    void Adrian::mover(sf::Vector2f direccion, float dt)
-    {
-        sprite.move(direccion * velocidad * dt);
-    }
-    void Adrian::dibujar
+// Movimiento absoluto
+void Adrian::setPosicion(sf::Vector2f posicion)
+{
+    sprite.setPosition(posicion);
+}
+
+// Movimiento relativo (llamar cada frame con dt)
+void Adrian::mover(sf::Vector2f direccion, float dt)
+{
+    sprite.move(direccion * velocidad * dt);
+}
+void Adrian::dibujar(sf::RenderWindow &ventana) const
+{
+    ventana.draw(sprite);
+}
